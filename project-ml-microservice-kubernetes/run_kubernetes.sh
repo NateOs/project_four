69e10736b74fab660fbd04b85ad3f7d7b8912c18
nateos <sodjanathan@gmail.com>
2022-08-06 15:53:22 +0100

    run kubernetes docker

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=nate233/ml
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml --image=$dockerpath 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml 8000:80
